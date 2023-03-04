403222599ee7d59726946bd60a98044cd096c34a
Lepommejamez <98928651+Lepommejamez@users.noreply.github.com>
2023-03-04 12:06:11 -0500

    Primer Commit

---
+++ Test-Arboles/src/main/java/com/mycompany/test/arboles/Arbol.java
@@ -0,0 +1,20 @@
+public class Arbol {
+    private Nodo root;
+    
+    public Arbol(){
+        this.root =null;
+    }
+
+    public void insert(int data)
+    {
+       
+    }
+    public Nodo getRoot(){
+        return root;
+    }
+
+    public void setRoot(Nodo root) 
+    {
+        this.root = root;
+    }
+}
---
+++ Test-Arboles/src/main/java/com/mycompany/test/arboles/Nodo.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Nodo {
+    private String nombre;
+    ArrayList<Nodo> hijos = new ArrayList<>();
+    
+    public Nodo(String data){
+        this.nombre = data;
+    }
+
+    public String getData(){
+        return nombre;
+    }
+
+    public Nodo getChild(int i)
+    {
+        try
+        {
+            return hijos.get(i);
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+    }
+    
+    public ArrayList getChildren()
+    {
+        return hijos;
+    }
+    
+    public void addChild(Nodo nodo)
+    {
+        hijos.add(nodo);
+    }
+    
+    public void addChild(String text)
+    {
+        Nodo node = new Nodo(text);
+        addChild(node);
+    }
+}
